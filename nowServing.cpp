c5c7abcbb9d83953100f6d21b774bc5c36b94ce3
NR5P <orangev8zcar@gmail.com>
2019-05-18 15:31:02 -0500

    now it's popping correctly at least

--- nowServing.cpp
+++ nowServing.cpp
@@ -23,9 +23,9 @@ bool isTherePriority = false;
 
 void checkIfTimeOut()
 {
-   if(helpRequestDequeue.front().getMinutesRemaining() < 1)
+   if(helpRequestDequeue.front().getMinutesRemaining() - minutesSince < 1)
       helpRequestDequeue.pop_front();
-   else if(helpRequestDequeue.back().getMinutesRemaining() < 1)
+   else if(helpRequestDequeue.back().getMinutesRemaining() - minutesSince < 1)
    {
       helpRequestDequeue.pop_back();
       isTherePriority = false;
